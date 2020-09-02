a5f69c927e5bb398c8a70426c0dabd336a064eed
Gage <gagemigan@gmail.com>
2020-09-02 10:56:48 -0400

    added functions and event handalers for /learn
    only thing left to do at this point is getting the more detailed
    functions of jumping from one word to another working.

--- src/contexts/WordContext.js
+++ src/contexts/WordContext.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 
 const nullWord = {
-  nextWord: null,
-  totalScore: null,
-  wordCorrectCount: null,
-  wordIncorrectCount: null,
+  correct_count : null,
+  id : null,
+  in_row : null, 
+  incorrect_count : null,
+  lang : {
+    name : null , 
+    total_score : null,
+  },
+  language_id : null,
+  memory_value : null,
+  next : null,
+  original : null,
+  translation : null ,
 };
 
 const WordContext = React.createContext({
   error: null,
   word: nullWord,
+  wordInput : null, 
   setError: () => { },
   clearError: () => { },
   setWord: () => { },
   clearWord: () => { },
+  handleWordInputChange : () => { },
 });
 
 export default WordContext;
@@ -24,6 +35,7 @@ export class WordProvider extends Component {
     this.state = {
       error: null,
       word: nullWord,
+      wordInput : '',
     };
   }
 
@@ -31,19 +43,27 @@ export class WordProvider extends Component {
     console.error(error);
     this.setState({ error });
   };
+  
   clearError = () => this.setState({ error: null });
 
   setWord = word => this.setState({ word });
+
   clearWord = () => this.setState({ word: nullWord });
+
+  handleWordInputChange = value => this.setState({wordInput : value})  
+
+  clearWordInput = () => this.setState({wordInput : ''})
 
   render() {
     const value = {
       error: this.state.error,
       word: this.state.word,
+      wordInput : this.state.wordInput,
       setError: this.setState,
       clearError: this.clearError,
       setWord: this.setWord,
-      clearWord: this.clearWord
+      clearWord: this.clearWord,
+      handleWordInputChange : this.handleWordInputChange,
     };
 
     return (
--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 import LanguageApiService from '../../services/language-api-service';
 import WordContext from '../../contexts/WordContext';
+import ProgressBar from '../../components/ProgressBar/ProgressBar'
 
 class LearningRoute extends Component {
-  static createContext = WordContext;
+  static contextType = WordContext;
 
   componentDidMount = () => {
     /**
      * @TODO tomorrow pass in the current word's next id 
      */
+    
 
     LanguageApiService.getNextWord(1)
       .then(res => {
-        console.log(res);
+        this.context.setWord(res)
       });
   };
 
+  handleInputChange = (ev) =>{
+    this.context.handleWordInputChange(ev.target.value)
+  }
+
+
+
   render() {
+    const { word } = this.context;
     return (
       <section>
+        <ProgressBar
+        // progress={/*something */}
+        />
         <h2>What is the translation for this word?</h2>
-        <div>
-          <span></span>
-        </div>
+          <div className='questions_wrapper'>
+            <label htmlFor='original_word'>{word.lang.name}</label>
+            <p
+            name='original_word'  
+            className='lang_question'>
+              {word.original}
+            </p>
+            <label htmlFor='eng_word_input'>english</label>
+            <input
+             onChange={(e) => this.handleInputChange(e)}
+             value={this.context.wordInput} 
+             name='eng_word_input' 
+             className='lang_question'
+             required ></input>
+          </div>
+          <div className='submission_info_wrapper'>
+            <p className='total_correct_for_word submit_info'>you have answered this word correctly {this.context.word.correct_count} times. </p>
+            <p className='total_incorrect_for_word submit_info'>You have answered this word incorrectly {this.context.word.incorrect_count} times. </p>
+            <button>Submit</button>
+          </div>
       </section>
     );
   }
