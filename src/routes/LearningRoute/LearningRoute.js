2cb87f4330b2055279462c9df6ca86ac91e5351f
nickthorpe71 <nickthorpe71@gmail.com>
2020-09-04 10:23:59 -0500

    finishes basic applciation functionality and all tests passing

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -21,9 +21,9 @@ class LearningRoute extends Component {
   handleSubmit = async (e) => {
     e.preventDefault();
 
-    const {wordInput} = this.context;
+    const { wordInput } = this.context;
 
-    this.context.setGuess(wordInput)
+    this.context.setGuess(wordInput);
 
     const responseObj = await LanguageApiService.submitGuess(
       wordInput
@@ -34,13 +34,17 @@ class LearningRoute extends Component {
 
     this.context.clearWordInput();
 
-    LanguageApiService.getNextWord().then((res) => {
-      this.context.setWord(res);
-    });
+    // LanguageApiService.getNextWord().then((res) => {
+    //   this.context.setWord(res);
+    // });
   };
 
   handleContinue = () => {
     console.log("continue");
+    this.setState({ submitted: false });
+    LanguageApiService.getNextWord().then((res) => {
+      this.context.setWord(res);
+    });
   };
 
   renderButton = () => {
@@ -52,7 +56,7 @@ class LearningRoute extends Component {
       );
     } else {
       return (
-        <button className="submission_button" onClick={this.handleContinue}>
+        <button className="submission_button" type="none" onClick={this.handleContinue}>
           Try another word!
         </button>
       );
@@ -62,55 +66,70 @@ class LearningRoute extends Component {
   renderResponse = () => {
     if (this.state.submitted) {
       if (this.context.resObj.isCorrect) {
-        return(
+        return (
           <>
             <h2>You were correct! :D</h2>
             <div className='DisplayFeedback'>
-                  <p>The correct translation for {this.context.word.nextWord} was {this.context.resObj.answer} and you chose {this.context.theLastGuess}!</p>
+              <p>The correct translation for {this.context.word.nextWord} was {this.context.resObj.answer} and you chose {this.context.theLastGuess}!</p>
             </div>
           </>
-        
-          
-          ) ;
+        );
       } else {
-            return( <>
-                     <div className='feedback_wrapper'> <h2 className='feedBack'>Good try, but not quite right :(</h2></div>
-                      <div className='DisplayFeedback'>
-                        <p>The correct translation for {this.context.word.nextWord} was {this.context.resObj.answer} and you chose {this.context.theLastGuess}!</p>
-                      </div>
-                    </>);
+        return (<>
+          <div className='feedback_wrapper'> <h2 className='feedBack'>Good try, but not quite right :(</h2></div>
+          <div className='DisplayFeedback'>
+            <p>The correct translation for {this.context.word.nextWord} was {this.context.resObj.answer} and you chose {this.context.theLastGuess}!</p>
+          </div>
+        </>);
       }
+    }
+  };
+
+  renderInputSection = () => {
+    if (!this.state.submitted) {
+      return (
+        <div className="eng_word_input">
+          <label htmlFor="learn-guess-input">
+            What's the translation for this word?
+              </label>
+          <input
+            onChange={(e) => this.handleInputChange(e)}
+            id="learn-guess-input"
+            type="text"
+            value={this.context.wordInput}
+            name="learn-guess-input"
+            className="lang_question"
+            required
+          ></input>
+        </div>
+      );
+    }
+  };
+
+  renderTranslateWord = () => {
+    let nextWord = this.context.resObj.nextWord ? this.context.resObj.nextWord : this.context.word.nextWord;
+    if (!this.state.submitted) {
+      return (
+        <div className='translated_word_wrapper'><h2 className="translated_word">Translate the word:</h2><span>{nextWord}</span></div>
+      );
     }
   };
 
   render() {
     const { word, resObj } = this.context;
 
-    let totalScore = resObj.totalScore ? resObj.totalScore : word.totalScore ;
+    let totalScore = resObj.totalScore ? resObj.totalScore : word.totalScore;
 
     return (
-    
+
       <section className="learning_section">
         {this.context.error && <p className='error'>{this.context.error}</p>}
         <div className="DisplayScore"><p>Your total score is: {totalScore}</p></div>
-        <div className='translated_word_wrapper'><h2 className="translated_word">Translate the word:</h2><span>{this.context.word.nextWord}</span></div>
+        {this.renderTranslateWord()}
         <form onSubmit={(e) => this.handleSubmit(e)}>
           <div className="questions_wrapper">
             <div className="lang_container">
-              <label htmlFor="learn-guess-input">
-                What's the translation for this word?
-              </label>
-              <div className="eng_word_input">
-                <input
-                  onChange={(e) => this.handleInputChange(e)}
-                  id="learn-guess-input"
-                  type="text"
-                  value={this.context.wordInput}
-                  name="learn-guess-input"
-                  className="lang_question"
-                  required
-                ></input>
-              </div>
+              {this.renderInputSection()}
               {this.renderResponse()}
             </div>
           </div>
